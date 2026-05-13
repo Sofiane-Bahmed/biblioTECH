e3945b278a25f868549f977ff282e1fc19d0d593
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2026-05-13 08:16:09 +0200

    refactor(db): replace try-catch block with asyncHandler

--- db/index.js
+++ db/index.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    mongoose.set("strictQuery", true);
-    const conn = await mongoose.connect(process.env.DBURI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1); // Stop the app if DB fails
-  }
-};
+import asyncHandler from "../utils/asyncHandler.js"
+
+const connectDB = asyncHandler(async () => {
+
+  mongoose.set("strictQuery", true);
+  const conn = await mongoose.connect(process.env.DBURI);
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+});
 
 export default connectDB;
--- routers/auth.js
+++ routers/auth.js
@@ -21,10 +21,11 @@ export const authRouter = express.Router()
 
 authRouter.post("/register", validate(registerSchema), register)
 authRouter.post("/login", validate(loginSchema), login)
+authRouter.patch("/reset-password/:token", validate(resetPasswordSchema), resetPassword)
+
 authRouter.get("/logout", logout)
 authRouter.post("/refresh", refresh)
 authRouter.post("/forgot-password", validate(forgotPasswordSchema), forgotPassword)
-authRouter.patch("/reset-password/:token", validate(resetPasswordSchema), resetPassword)
 
 
 
