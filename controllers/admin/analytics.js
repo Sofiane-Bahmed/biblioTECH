59ffe29f2a42bf752ff9507c5a7a071e99845a00
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2026-05-31 10:16:10 +0200

    perf(analytics): rewrite statistics engine using native mongo aggregation pipelines and concurrent execution

--- controllers/admin/analytics.js
+++ controllers/admin/analytics.js
@@ -5,35 +5,44 @@ import asyncHandler from "../../utils/async-handler.js";
 // view libary statistics
 export const getLibraryStatistics = asyncHandler(async (req, res) => {
 
-    const borrows = await BorrowBook.find();
-    const books = await Book.find();
+    const [bookCount, borrowCount, topBookStats] = await Promise.all([
+        Book.countDocuments(),
+        BorrowBook.countDocuments(),
+        BorrowBook.aggregate([
+            {
+                $group: {
+                    _id: "$book",
+                    count: { $sum: 1 }, // Track the number of occurrences
+                },
+            },
+            { $sort: { count: -1 } },
+            { $limit: 1 }, // Snag just the absolute #1 record
+            {
+                $lookup: {
+                    from: "books", // Join with the books collection
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "bookDetails",
+                },
+            },
+            { $unwind: "$bookDetails" },
+            {
+                $project: {
+                    _id: 0,
+                    id: "$_id",
+                    count: 1,
+                    title: "$bookDetails.title",
+                    author: "$bookDetails.author",
+                },
+            },
+        ]),
+    ]);
 
-    const borrowCount = borrows.length;
-    const bookCount = books.length;
+    const mostBorrowedBook = topBookStats[0] || { id: null, count: 0, title: "No history yet" };
 
-    let mostBorrowedBook = { id: null, count: 0 };
-    const bookCounts = {};
-
-    for (const borrow of borrows) {
-        const bookId = borrow.book;
-        if (bookId in bookCounts) {
-            bookCounts[bookId]++;
-        } else {
-            bookCounts[bookId] = 1;
-        }
-
-        if (bookCounts[bookId] > mostBorrowedBook.count) {
-            mostBorrowedBook = { id: bookId, count: bookCounts[bookId] };
-        }
-    }
-
-    const statistics = {
-        borrowCount,
+    res.status(200).json({
         bookCount,
+        borrowCount,
         mostBorrowedBook,
-        bookCounts
-    };
-
-    res.status(200).json(statistics);
-
-}); 
+    });
+});
